93e818f26e7f6e80719b1014ed44457f66ac1f0b
Mihai Luca <mihailuca406@gmail.com>
2019-04-05 19:59:27 +0300

    created roughly equivalent D program (using regex)

--- Word Frequency/frequencies.d
+++ Word Frequency/frequencies.d
@@ -8,31 +8,31 @@ void main() {
     // Compute counts
     uint[string] freqs;
     uint total_words;
-    auto r = regex("[ \\d\\[\\]{}()\t,.;:!?\"'~`/+*$]+(-{2,})('s)");
+    auto r = regex("('s)*-*'*-*[ \\\\\\d\\[\\]{}()\t,.;:!?\"~`/+*$]+-*'*-*|'*-{2,}'*|'s$|^-*'+|'+-* +|'+-*$|^-+|-+ +|-+$");
     foreach (line; stdin.byLine()) {
         foreach (word; split(toLower(strip(line)), r)) {
-            ++freqs[word.idup];
+            if (word.length) ++freqs[word.idup];
             ++total_words;
         }
     }
     // Summary
     double avg_freq = double(total_words)/freqs.length;
-    writeln("Total words: ", total_words);
-    writeln("Different words: ", freqs.length);
-    writeln("Average frequency: ", avg_freq);
-    writeln("\n----\n");
+    writeln("Total number of words: ", total_words);
+    writeln("Number of different words: ", freqs.length);
+    writeln("Average word frequency: ", avg_freq);
+    writeln("\n-----\n");
     // Print counts
     string[] words = freqs.keys;
     // Aphabetically
     sort!((a, b) { return a < b; }) (words);
     foreach (word; words) {
-        writefln("%s\t% u", word, freqs[word]);
+        writefln("%s %u", word, freqs[word]);
     }
     // Separator
-    writeln("\n----\n");
+    writeln("\n-----\n");
     // By frequency
     sort!((a, b) { return freqs[a] > freqs[b]; }) (words);
     foreach (word; words) {
-        writefln("%6u\t%s", freqs[word], word);
+        writefln("%s %u", word, freqs[word]);
     }
 }
