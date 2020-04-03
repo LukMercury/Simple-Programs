e91649eb59724af86879c8c53a827705fcad5ec6
Mihai Luca <mihailuca406@gmail.com>
2020-04-03 21:51:17 +0300

    change case scripts

---
+++ Change Case/README.md
@@ -0,0 +1,11 @@
+#### Change case of all characters of files 
+### Bash shell script
+
+---
+
+tolower.py <file1> <file2> ...
+toupper.py <file1> <file2> ...
+
+works with * wildcard because of shell expansion
+
+---
---
+++ Change Case/toupper.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+
+import os, sys
+
+files = []
+
+for file in sys.argv[:]:
+	if os.path.isfile(file):
+		files.append(file)
+
+renamed = []
+
+for file in files:
+	renamed.append(file.upper())
+
+dictionary  = dict(zip(files, renamed))
+
+for file, renamed in dictionary.items():
+	os.rename(file, renamed)
