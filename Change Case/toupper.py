ad7e58f1fa632adb31343248387874d612c04ab4
Mihai Luca <mihailuca406@gmail.com>
2020-04-03 22:16:17 +0300

    fixed scripts

--- Change Case/toupper.py
+++ Change Case/toupper.py
@@ -4,8 +4,13 @@ import os, sys
 
 files = []
 
-for file in sys.argv[:]:
-	if os.path.isfile(file):
+cwd = os.environ['PWD']
+
+if len(sys.argv) == 0:
+	sys.exit()
+
+for file in sys.argv[1:]:
+	if os.path.isfile(os.path.join(cwd, file)):
 		files.append(file)
 
 renamed = []
@@ -16,4 +21,4 @@ for file in files:
 dictionary  = dict(zip(files, renamed))
 
 for file, renamed in dictionary.items():
-	os.rename(file, renamed)
+	os.rename(os.path.join(cwd, file), os.path.join(cwd, renamed))
